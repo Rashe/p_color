06b578454036a01b6c4c443cf25a15e179a2d583
rashe <rafael@withminta.com>
2020-12-03 15:38:13 +0200

    pip

--- meme.md
+++ meme.md
@@ -1,4 +1,4 @@
 python3 setup.py bdist_wheel
-python3 setup.py bdist_wheel
+python3 setup.py bdist
 
 python3 -m twine upload --repository pypi dist/*
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='p_color',
-    version='0.1.3',
+    version='0.1.4',
     author="Rashe",
     description="Color your print()",
     long_description=open("README.md").read(),
